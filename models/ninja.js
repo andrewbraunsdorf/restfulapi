8c026023dffab8d92f674411a4a752d4529c7602
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-20 23:27:04 +0000

    created new file called ninja, requiring mongoose, and created models using schemas

---
+++ models/ninja.js
@@ -0,0 +1,23 @@
+const mongoose = require("monngoose");
+const Schema = mongoose.Schema;
+
+//create ninja Schema & model
+const NinjaSchema = new Schema ({
+	name: {
+		type: String,
+		required: [true, "Name field is required"]
+	},
+	rank: {
+		type: String
+	},
+	avaiable: {
+		type: Boolean,
+		default: false
+	}
+	// add in geolocation
+});
+
+const Ninja = mongoose.model("ninjatte", NinjaSchema);
+
+module.exports = Ninja; 
+
