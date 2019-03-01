bf4307fbbcc7dea062564543f2a6203eebd11d13
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-01 00:57:19 +0000

    added notes, catch method and next to them

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 //set up express app
 const app = express();
@@ -14,6 +14,12 @@ app.use(bodyParser.json());
 
 //initialize routes
 app.use("/api", require("./routes/api"));
+
+//error handling middleware
+app.use(function(err, req, res, next){
+	// console.log(err);
+	res.status(422).send({error: err.message});
+});
 
 //listen for requests
 app.listen(process.env.port || 8080, function(){
